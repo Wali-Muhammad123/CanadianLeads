c0ae8c410356960d321b910531af873ab51d9b07
Wali-Muhammad123 <renedescartian@gmail.com>
2023-02-24 16:46:49 +0500

    Created basic files necessary

--- kijicrawler/kijicrawler/items.py
+++ kijicrawler/kijicrawler/items.py
@@ -6,7 +6,12 @@
 import scrapy
 
 
-class KijicrawlerItem(scrapy.Item):
+class LeadItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name=scrapy.Field()
+    phone=scrapy.Field()
+    address=scrapy.Field()
+    dateposted=scrapy.Field()
+    def __str__(self):
+        return str(self['name']) + str(self['phone']) + str(self['address']) + str(self['dateposted'])
